15a79d0b78efeb9167b1b24955a4910a310eff0b
Bogdan <zarechnyy00@internet.ru>
2025-04-12 13:12:55 +0300

    Fix base path and deployment configuration

--- online-store/vite.config.js
+++ online-store/vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/vue-projects/online-store/',
+  base: '/Vue-projects/online-store/',
 })
