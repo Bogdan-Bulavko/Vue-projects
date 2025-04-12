e53f27e70a705611df65dc265af414ad68cd6ea6
Bogdan <zarechnyy00@internet.ru>
2025-04-12 18:15:03 +0300

    Update project

--- online-store/vite.config.js
+++ online-store/vite.config.js
@@ -14,5 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/Vue-projects/online-store',
+  base: '/Vue-projects/online-store/',
 })
